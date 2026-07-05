c7b3b193df18cb65c1314383859833d2a780106c
breadstalker <johnsavioromy@gmail.com>
2026-07-05 17:28:07 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,19 @@
+variable = 10
+
+print(type(variable))
+
+variable = 12.36
+
+print(type(variable))
+
+variable = True
+
+print(type(variable))
+
+variable = 'John'
+
+print(type(variable))
+
+variable = [1, 2, 3]
+
+print(type(variable))
